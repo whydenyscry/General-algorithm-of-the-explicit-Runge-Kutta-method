55e2fa180702dbd12aa3beda8c01e01ff5ce0a0a
whydenyscry <gogool124512@gmail.com>
2024-04-11 20:58:35 +0300

    add script

---
+++ odeExplicitGeneral.m
@@ -0,0 +1,31 @@
+function [t, xsol] = odeExplicitGeneral(c_vector,A_matrix,b_vector,fun,timeint,tau,incond)
+
+s_stages = length(c_vector);
+m = length(incond);
+
+incond = reshape(incond,[m 1]);
+    
+
+t = (timeint(1):tau:timeint(2))';
+xsol = zeros(length(incond), length(t));
+xsol(:, 1) = incond(:);
+
+for n = 1:length(t)-1
+    t_n = t(n);
+    x_n = xsol(:, n);
+    tau_n = tau;
+    
+    K_matrix = zeros(m, s_stages);
+    K_matrix(:, 1) = fun(t_n, x_n);
+    
+        for i = 2:s_stages
+            t_arg = t_n + tau_n * c_vector(i);
+            a_vector = A_matrix(i, 1:i-1)';
+            x_arg = x_n + tau_n * K_matrix(:, 1:i-1) * a_vector;
+            K_matrix(:, i) = fun(t_arg, x_arg);
+        end
+    
+    xsol(:, n+1) = x_n + tau_n * K_matrix * b_vector;
+end
+xsol = xsol';
+end
