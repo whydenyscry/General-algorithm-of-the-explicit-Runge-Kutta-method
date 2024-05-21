9cc8835250b330abeff22dd0c3fb43ad0d61fbf3
whydenyscry <gogool124512@gmail.com>
2024-05-22 01:01:48 +0300

    Update odeExplicitGeneral.m
    rename some arguments

--- odeExplicitGeneral.m
+++ odeExplicitGeneral.m
@@ -1,4 +1,4 @@
-function [t, xsol] = odeExplicitGeneral(c_vector,A_matrix,b_vector,fun,timeint,tau,incond)
+function [t, xsol] = odeExplicitGeneral(c_vector, A_matrix, b_vector, odefun, tspan, tau, incond)
 
 s_stages = length(c_vector);
 m = length(incond);
@@ -7,7 +7,7 @@ c_vector = reshape(c_vector, [s_stages 1]);
 b_vector = reshape(b_vector, [s_stages 1]);
 incond = reshape(incond, [m 1]);
 
-t = (timeint(1) : tau : timeint(2))';
+t = (tspan(1) : tau : tspan(2))';
 xsol = zeros(length(incond), length(t));
 xsol(:, 1) = incond(:);
 
@@ -17,13 +17,13 @@ for n = 1:length(t)-1
     tau_n = tau;
     
     K_matrix = zeros(m, s_stages);
-    K_matrix(:, 1) = fun(t_n, x_n);
+    K_matrix(:, 1) = odefun(t_n, x_n);
     
         for i = 2:s_stages
             t_arg = t_n + tau_n * c_vector(i);
             a_vector = A_matrix(i, 1:i-1)';
             x_arg = x_n + tau_n * K_matrix(:, 1:i-1) * a_vector;
-            K_matrix(:, i) = fun(t_arg, x_arg);
+            K_matrix(:, i) = odefun(t_arg, x_arg);
         end
     
     xsol(:, n+1) = x_n + tau_n * K_matrix * b_vector;
